90cdf8e438784e77ff2ee7ce974bc2fcbb7af6f5
mulion <ivannemchinovm@yandex.ru>
2025-05-18 14:52:56 +0300

    less

--- src/main/java/com/mulion/data_base/services/DBBoatService.java
+++ src/main/java/com/mulion/data_base/services/DBBoatService.java
@@ -1,11 +1,8 @@
 package com.mulion.data_base.services;
 
-import com.mulion.data_base.repository.Repository;
+import com.mulion.data_base.repositories.Repository;
 import com.mulion.models.Boat;
-import com.mulion.models.User;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.Hibernate;
-import org.hibernate.Session;
 
 import java.util.List;
 
