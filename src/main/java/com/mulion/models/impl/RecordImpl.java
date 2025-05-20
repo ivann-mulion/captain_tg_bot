e455eea371974c942c0c580e39262c158bfacfe6
mulion <ivannemchinovm@yandex.ru>
2025-05-20 19:44:08 +0300

    update old RecordImpl - remove shit logic

--- src/main/java/com/mulion/models/impl/RecordImpl.java
+++ src/main/java/com/mulion/models/impl/RecordImpl.java
@@ -8,19 +8,20 @@ import lombok.Data;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static com.mulion.constants.Config.yclientDateFormatter;
 
 @Data
 public class RecordImpl implements Record {
+    public static final String PRE = "пред : ";
+    public static final String BOAT = "борт : ";
+    public static final String AQ = "экв  : ";
+    public static final String FREE = "бюджет";
     private final LocalDate date;
     private final LocalTime startTime;
     private final LocalTime endTime;
     private final int length;
-    private String paymentInfo;
     private int prepayment;
     private int acquiring;
     private int cash;
@@ -31,47 +32,43 @@ public class RecordImpl implements Record {
         startTime = dateTime.toLocalTime();
         length = dataRecord.getLength();
         endTime = startTime.plusSeconds(length);
-        paymentInfo = createPaymentInfo(transactions);
+        createPaymentInfo(transactions);
     }
 
     @Override
     public String toString() {
-        return String.format("%s - %s, %s", startTime, endTime, paymentInfo);
+        return String.format("%s - %s, %s", startTime, endTime, paymentInfo());
     }
 
-    private String createPaymentInfo(List<PaymentTransactionRecord> transactionRecords) {
-        if (transactionRecords == null) return "";
-        if (transactionRecords.isEmpty()) return "бюджет";
-        Map<String, Integer> paymants = new HashMap<>();
-        String pre = "пред : ";
-        String boat = "борт : ";
-        String aq = "экв  : ";
+    private String paymentInfo() {
+        StringBuilder result = new StringBuilder();
+        if (prepayment > 0) {
+            result.append(PRE).append(prepayment).append(' ');
+        }
+        if (acquiring > 0) {
+            result.append(AQ).append(acquiring).append(' ');
+        }
+        if (cash > 0) {
+            result.append(BOAT).append(cash).append(' ');
+        }
+        if (result.isEmpty()) {
+            result.append(FREE);
+        }
+        return result.toString();
+    }
+
+    private void createPaymentInfo(List<PaymentTransactionRecord> transactionRecords) {
+        if (transactionRecords == null || transactionRecords.isEmpty()) return;
         if (!transactionRecords.getFirst().getAccount().isCash() && transactionRecords.size() != 1) {
-            paymants.put(pre, transactionRecords.getFirst().getAmount());
             prepayment += transactionRecords.getFirst().getAmount();
             transactionRecords.removeFirst();
         }
         for (PaymentTransactionRecord transaction : transactionRecords) {
             if (transaction.getAccount().isCash()) {
-                paymants.merge(boat, transaction.getAmount(), Integer::sum);
                 cash += transaction.getAmount();
             } else {
-                paymants.merge(aq, transaction.getAmount(), Integer::sum);
                 acquiring += transaction.getAmount();
-
             }
         }
-        StringBuilder result = new StringBuilder();
-        Integer amount;
-        if ((amount = paymants.get(pre)) != null) {
-            result.append(pre).append(amount).append(' ');
-        }
-        if ((amount = paymants.get(aq)) != null) {
-            result.append(aq).append(amount).append(' ');
-        }
-        if ((amount = paymants.get(boat)) != null) {
-            result.append(boat).append(amount).append(' ');
-        }
-        return result.toString();
     }
 }
